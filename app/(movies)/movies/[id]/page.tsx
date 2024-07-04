b00bb4069661142f7101e6adf18d829344b85008
Jisung Lee <jskurt9352@gmail.com>
2024-07-04 13:32:48 +0900

    add dynamic metadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,18 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default function MovieDetail({
-  params: { id },
-}: {
+interface IParamsProps {
   params: { id: string };
-}) {
+}
+export async function generateMetadata({ params: { id } }: IParamsProps) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default function MovieDetail({ params: { id } }: IParamsProps) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
